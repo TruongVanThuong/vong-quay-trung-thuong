c3caa3b033251393e90cf9ed1dd6068b93659057
TruongVanThuong <tvt230101@gmail.com>
2024-05-10 17:14:02 +0700

    first

---
+++ assets/js/main.js
@@ -0,0 +1,128 @@
+// main.js
+
+
+const loginForm = document.querySelector('.login-form');
+
+const colors = ['#feca54', '#fff3cd', '#feca54', '#fff3cd', '#feca54', '#fff3cd', '#feca54', '#fff3cd'];
+const listGift = [
+    {
+        name: 'Bút bi',
+        percent: 99 / 100,
+        image: './assets/img/product-1.png',
+    },
+    {
+        name: 'Sổ tay',
+        percent: 1 / 100,
+        image: './assets/img/product-2.png',
+    },
+    {
+        name: 'Ba lô',
+        percent: 0 / 100,
+        image: './assets/img/product-3.png',
+    },
+    {
+        name: 'Nón bảo hiểm',
+        percent: 0 / 100,
+        image: './assets/img/product-4.png',
+    },
+    {
+        name: 'Bút bi',
+        percent: 0 / 100,
+        image: './assets/img/product-1.png',
+    },
+    {
+        name: 'Sổ tay',
+        percent: 0 / 100,
+        image: './assets/img/product-2.png',
+    },
+    {
+        name: 'Ba lô',
+        percent: 0 / 100,
+        image: './assets/img/product-3.png',
+    },
+    {
+        name: 'Nón bảo hiểm',
+        percent: 0 / 100,
+        image: './assets/img/product-4.png',
+    },
+];
+(() => {
+    const $ = document.querySelector.bind(document);
+    const wheel = $('.wheel');
+    const btnSpin = $('.spin-btn');
+    let timer = 7000; // Thời gian cho mỗi lần quay
+    let isRotating = false; // Đang quay hay không?
+    let currentRotate = 0;
+    const giftSize = listGift.length;
+    const rotate = 360 / giftSize;
+    const leftStyle = rotate/giftSize;
+    const bottomStyle = rotate/2;
+    const skewY = 90 - rotate; // Độ nghiêng của 1 item
+    const renderGift = () => {
+        listGift.forEach((item, index) => {
+            const itemGift = document.createElement('li');
+            itemGift.style.transform = `rotate(${rotate * index}deg) skewY(-${skewY}deg)`;
+            itemGift.innerHTML = `
+                <p class="text-item" style="
+                    background-color: ${colors[index % colors.length]};
+                    transform: skewY(${skewY}deg) rotate(${rotate / 2}deg);
+                ">
+
+                </p>
+                <img class="wheel-img" src="${item.image}"
+                    style="
+                        left: ${leftStyle}%;
+                        bottom: ${bottomStyle}%;
+                        transform: skewY(${skewY}deg)
+                    " />
+            `;
+            wheel.appendChild(itemGift);
+        })
+    }
+    const rotateWheel = (currentRotate, index) => {
+        wheel.style.transform = `rotate(${currentRotate - index * rotate - rotate / 2}deg)`;
+    }
+    const getGift = (randomNumber) => {
+        let currentPercent = 0;
+        let list = [];
+        listGift.forEach((item, index) => {
+            currentPercent += item.percent;
+            randomNumber <= currentPercent && list.push({
+                ...item, index
+            });
+        });
+        return list[0];
+    }
+    const showGift = (gift) => {
+        setTimeout(() => {
+            isRotating = false;
+            Swal.fire({
+                title: 'Chúc mừng bạn đã trúng ' + gift.name,
+                imageUrl: gift.image,
+                imageHeight: 200
+            })
+        }, timer);
+    }
+    const spinner = () => {
+        isRotating = true;
+        const gift = getGift(Math.random());
+        currentRotate += 360 * 10;
+        rotateWheel(currentRotate, gift.index);
+        showGift(gift);
+    }
+    btnSpin.addEventListener('click', () => {
+        if (!isLoggedIn) {
+            // alert('chua dang nhap')
+            loginForm.style.display = 'flex';
+        } else {
+            !isRotating && spinner();
+        }
+    });
+    renderGift();
+})();
+
+function resize() {
+    var width = $(window).width();
+    document.documentElement.style.setProperty('--size', width > 600 ? "500px" : (width / 1.1 - 32) + "px");
+}
+resize();
---
+++ assets/js/validation.js
@@ -0,0 +1,80 @@
+// validation.js
+let isLoggedIn = false;
+
+
+const usernameEle = document.getElementById('username');
+const emailEle = document.getElementById('email');
+const phoneEle = document.getElementById('phone');
+
+const btnLogin = document.getElementById('btn-login');
+const inputEles = document.querySelectorAll('.input-row');
+
+btnLogin.addEventListener('click', function () {
+    Array.from(inputEles).map((ele) =>
+        ele.classList.remove('success', 'error')
+    );
+    let isValid = checkValidate();
+
+    if (isValid) {
+        loginForm.style.display = 'none';
+        isLoggedIn = true;
+        alert('login thành công');
+    }
+});
+
+function checkValidate() {
+    let usernameValue = usernameEle.value;
+    let emailValue = emailEle.value;
+    let phoneValue = phoneEle.value;
+
+    let isCheck = true;
+
+    if (usernameValue == '') {
+        setError(usernameEle, 'Tên không được để trống');
+        isCheck = false;
+    } else {
+        setSuccess(usernameEle);
+    }
+
+    if (emailValue == '') {
+        setError(emailEle, 'Email không được để trống');
+        isCheck = false;
+    } else if (!isEmail(emailValue)) {
+        setError(emailEle, 'Email không đúng định dạng');
+        isCheck = false;
+    } else {
+        setSuccess(emailEle);
+    }
+
+    if (phoneValue == '') {
+        setError(phoneEle, 'Số điện thoại không được để trống');
+        isCheck = false;
+    } else if (!isPhone(phoneValue)) {
+        setError(phoneEle, 'Số điện thoại không đúng định dạng (035789****)');
+        isCheck = false;
+    } else {
+        setSuccess(phoneEle);
+    }
+
+    return isCheck;
+}
+
+function setSuccess(ele) {
+    ele.parentNode.classList.add('success');
+}
+
+function setError(ele, message) {
+    let parentEle = ele.parentNode;
+    parentEle.classList.add('error');
+    parentEle.querySelector('small').innerText = message;
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
+}
+
+function isPhone(number) {
+    return /(84|0[3|5|7|8|9])+([0-9]{8})\b/.test(number);
+}
